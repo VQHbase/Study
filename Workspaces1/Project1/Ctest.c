6a7d4f48fb9db3c82920cb3d61f1aff6b226fdbf
VQHbase <quanghuy.base.181201@gmail.com>
2026-01-05 03:34:14 +0000

    Test C compiler

---
+++ Workspaces1/Project1/Ctest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int myfavnum = 0;
+    
+    printf("enter your fav number\n");
+    scanf("%d", &myfavnum);
+    printf("your fav num is %d\n", myfavnum);
+    
+    return 0;
+}
